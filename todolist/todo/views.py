776e49b27f618226448dc534b789783c788b6a48
Pydi Venkata Surya Narayana Murthy <149566134+Balaji-dev-png@users.noreply.github.com>
2026-02-12 09:47:47 +0530

    modified

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task, Category
-from .forms import TaskForm # Assumes you have a CategoryForm if adding categories manually
+from .forms import TaskForm, CategoryForm 
 
-# 1. Main Page: List all Categories (Work Headings)
-from .forms import TaskForm, CategoryForm # Make sure both are imported
 
 def categorylist(request):
     categories = Category.objects.all()
@@ -18,11 +16,17 @@ def categorylist(request):
         
     return render(request, 'categorylist.html', {'categories': categories, 'form': form})
 
+
+
+
 def tasklist(request, category_id):
     category = get_object_or_404(Category, id=category_id)
-    # This specifically gets tasks linked to THIS category
     tasks = Task.objects.filter(name=category) 
     return render(request, 'tasklist.html', {'tasks': tasks, 'category': category})
+
+
+
+
 
 def createtask(request, category_id):
     category = get_object_or_404(Category, id=category_id)
@@ -30,7 +34,6 @@ def createtask(request, category_id):
         form = TaskForm(request.POST)
         if form.is_valid():
             task = form.save(commit=False)
-            # This line fixes the [null] in your database image
             task.name = category  
             task.save()
             return redirect('tasklist', category_id=category.id)
@@ -39,20 +42,30 @@ def createtask(request, category_id):
     return render(request, 'taskform.html', {'form': form, 'category': category})
 
 
+
+
+
+
+
+
+
 def deletecategory(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     
     if request.method == "POST":
-        category.delete()  # This physically removes it from the DB
-        return redirect('categorylist') # Redirect back to the main list
-    
-    # If it's a GET request, show the confirmation page
+        category.delete() 
+        return redirect('categorylist')
     return render(request, 'taskdelete.html', {'item': category, 'type': 'Category'})
 
-# 5. Delete Task: Your existing delete logic
+
+
+
+
+
+
 def deletetask(request, pk):
     task = get_object_or_404(Task, pk=pk)
-    category_id = task.name.id # Save ID to redirect back to the correct list
+    category_id = task.name.id 
     if request.method == 'POST':
         task.delete()
         return redirect('tasklist', category_id=category_id)
